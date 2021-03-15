bb40492067a1aad125198b3452badfbc90b9ab8b
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-03-15 01:29:35 -0400

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+alias cp='cp -rf'
+alias mv='mv -v'
+mkdir -p /opt/kernel-build
+cd /opt/kernel-build
+mkdir -p /root/rpmbuild
+mkdir -p /root/rpmbuild/SPECS
+mkdir -p /root/rpmbuild/SOURCES
+mkdir -p /root/rpmbuild/SRPMS
+mkdir -p /root/rpmbuild/RPMS
+mkdir -p /root/rpmbuild/RPMS/x86_64
+yum install nano wget xz rpm-build elfutils-libelf-devel openssl-devel openssl bc rsync -y
+yum groupinstall "C Development Tools and Libraries" -y
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.11.6.tar.xz
 tar -xvf linux-5.11.6.tar.xz
 mv linux-5.11.6 kernel-5.11.6_penguin_expSEHDsec+T
@@ -18,3 +30,4 @@ cd ../../../kernel-5.11.6_penguin_expSEHDsec+T/
 make -j24 rpm-pkg
 cd /root/rpmbuild/RPMS/x86_64/
 ls -lah
+
