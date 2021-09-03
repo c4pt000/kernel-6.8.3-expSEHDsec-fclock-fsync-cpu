28a22a1c0318a1f0c495a3c3ba477c795dd5f113
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-09-03 12:58:36 -0400

    Update BLUEZ-0bda:b00c-Realtek-rtl8723au_bt-installer.sh

--- BLUEZ-0bda:b00c-Realtek-rtl8723au_bt-installer.sh
+++ BLUEZ-0bda:b00c-Realtek-rtl8723au_bt-installer.sh
@@ -1,1 +1,16 @@
 https://github.com/c4pt000/rtl8723au_bt
+
+
+
+
+https://github.com/c4pt000/rtl88x2ce-dkms
+
+```
+0000:01:00.0 Network controller [0280]: Realtek Semiconductor Co., Ltd. RTL8822CE 802.11ac PCIe Wireless Network Adapter [10ec:c822]
+
+git clone https://github.com/juanro49/rtl88x2ce-dkms
+cd rtl88x2ce-dkms/
+make -j24
+make -j24 install
+modprobe rtl88x2ce
+```
