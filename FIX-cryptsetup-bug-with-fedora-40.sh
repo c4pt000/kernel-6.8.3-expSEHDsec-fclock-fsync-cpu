5e54c7f9de730dd9ea9edc96fd4cf1b8c2724458
https://bitnet-io.org <46433702+c4pt000@users.noreply.github.com>
2024-07-20 12:48:27 -0700

    Update FIX-cryptsetup-bug-with-fedora-40.sh

--- FIX-cryptsetup-bug-with-fedora-40.sh
+++ FIX-cryptsetup-bug-with-fedora-40.sh
@@ -6,4 +6,11 @@ chmod +x remove-cryptsetup.txt
 
 
 
-dnf install cryptsetup cryptsetup-libs --releasever=39 -y
+#dnf install cryptsetup cryptsetup-libs --releasever=39 -y
+
+dnf install cryptsetup* --releasever=34 --allowerasing -y
+
+
+echo "add an exclude line to your /etc/dnf/dnf/conf to prevent cryptsetup from reinstalling"
+
+echo "exclude=cryptsetup*"
