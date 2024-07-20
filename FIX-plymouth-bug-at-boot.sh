d8972f45acefb91c93e9cf26984378d61995ae06
https://bitnet-io.org <46433702+c4pt000@users.noreply.github.com>
2024-07-20 12:13:34 -0700

    Update FIX-plymouth-bug-at-boot.sh

--- FIX-plymouth-bug-at-boot.sh
+++ FIX-plymouth-bug-at-boot.sh
@@ -1,8 +1,4 @@
 rpm -qa | grep plymouth > remove-plymouth.txt
 sed -i 's/^/rpm -e --nodeps /g' remove-plymouth.txt 
 chmod +x remove-plymouth.txt 
-./remove-plymouth.txt 
-
-
-
-dnf install plymouth plymouth-core-libs plymouth-devel plymouth-graphics-libs plymouth-plugin* plymouth-scripts plymouth-system-theme plymouth-theme* --releasever=34 --allowerasing -y
+./remove-plymouth.txt
